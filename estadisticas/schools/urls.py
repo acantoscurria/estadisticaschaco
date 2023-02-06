c9727d4529a7f9159e546b9fe6a8949d19aeabeb
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-02-06 10:54:11 -0300

    funcionando graficos con datos ficticios

--- estadisticas/schools/urls.py
+++ estadisticas/schools/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from .views import OfferSelectionView
+from .views import OfferSelectionView,chart_data
 
 app_name = 'schools'
 
 urlpatterns = [
     path("offer-selection/",OfferSelectionView.as_view(),name="offer-selection"),
+    path("chart-data/<oferta>",chart_data,name="chart-data"),
 ]
--- estadisticas/schools/views.py
+++ estadisticas/schools/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 
 from users.models import User
 
@@ -13,4 +14,27 @@ class OfferSelectionView(LoginRequiredMixin,ListView):
     login_url = '/'
     
     def get_queryset(self):
-        return User.objects.filter(username=self.request.user.username)
+        return User.objects.get(username=self.request.user.username).offer_set.values('anexo','oferta')
+    
+
+
+# añadir login required y solo metodo get
+def chart_data(request,oferta=None):
+    if not oferta:
+        return JsonResponse("Es necesario algún valor.")
+    
+    oferta = oferta.split(" | ")
+    print(oferta)
+    cue = request.user.username
+    
+    data = {
+        "labels": ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        "datasets": [{
+            "label": "Ventas",
+            "data": [100, 200, 300, 400, 500],
+            "backgroundColor": ["red", "blue", "green", "yellow", "purple"]
+        }]
+    }
+
+
+    return JsonResponse(data)
