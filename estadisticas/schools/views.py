070ea3abc00629c391e5a37d8f467dfe9d9f8657
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-02-07 10:16:38 -0300

    falta separar grafico distintos labels

--- estadisticas/schools/views.py
+++ estadisticas/schools/views.py
@@ -33,11 +33,33 @@ def chart_data(request,oferta=None):
     
     options_desempenio={
         "Común - Secundaria Completa req. 7 años": {
-                                                    "table_name":"dech.desempenio_secundaria_ciencias_naturales",
-                                                    "des_name":"Desempeño Secundaria Ciencias Naturales",
-                                                    "col_name": "secundaria_desempenio_cn"
+                                                    "table_name_cn":"dech.desempenio_secundaria_ciencias_naturales",
+                                                    "des_name_cn":"Desempeño Secundaria Ciencias Naturales",
+                                                    "col_name_cn": "secundaria_desempenio_cn", 
+                                                    "table_name_cs":"dech.desempenio_secundaria_ciencias_sociales",
+                                                    "des_name_cs":"Desempeño Secundaria Ciencias Sociales",
+                                                    "col_name_cs": "secundaria_desempenio_cs", 
+                                                    "table_name_l":"dech.desempenio_secundaria_lengua",
+                                                    "des_name_l":"Desempeño Secundaria Ciencias Lengua",
+                                                    "col_name_l": "secundaria_desempenio_l", 
+                                                    "table_name_m":"dech.desempenio_secundaria_matematica",
+                                                    "des_name_m":"Desempeño Secundaria Ciencias Naturales",
+                                                    "col_name_m": "secundaria_desempenio_m",
                                                     },
-        "Común - Primaria de 7 años ": "dech.desempenio_primaria_ciencias_naturales",
+        "Común - Primaria de 7 años ": {
+                                                    "table_name_cn":"dech.desempenio_secundaria_ciencias_naturales",
+                                                    "des_name_cn":"Desempeño Secundaria Ciencias Naturales",
+                                                    "col_name_cn": "secundaria_desempenio_cn", 
+                                                    "table_name_cs":"dech.desempenio_secundaria_ciencias_sociales",
+                                                    "des_name_cs":"Desempeño Secundaria Ciencias Sociales",
+                                                    "col_name_cs": "secundaria_desempenio_cs", 
+                                                    "table_name_l":"dech.desempenio_secundaria_lengua",
+                                                    "des_name_l":"Desempeño Secundaria Ciencias Lengua",
+                                                    "col_name_l": "secundaria_desempenio_l", 
+                                                    "table_name_m":"dech.desempenio_secundaria_matematica",
+                                                    "des_name_m":"Desempeño Secundaria Ciencias Naturales",
+                                                    "col_name_m": "secundaria_desempenio_m",
+                                                    },
     }
     
     anexo,oferta = oferta.split(" | ")
@@ -45,11 +67,13 @@ def chart_data(request,oferta=None):
     cueanexo = request.user.username + anexo
 
     datasets = []
-    print(cueanexo,anexo,oferta,options_desempenio[oferta])
-    desempenio = DechDesempenio.objects.using("detch").raw("select * from dech.desempenio('{}','{}','{}')".format(cueanexo,options_desempenio.get(oferta).get("table_name"),options_desempenio.get(oferta).get("col_name")))
+    # print(cueanexo,anexo,oferta,options_desempenio[oferta])
+
+    desempenio_math = DechDesempenio.objects.using("detch").raw("select * from dech.desempenio('{}','{}','{}')".format(cueanexo,options_desempenio.get(oferta).get("table_name_m"),options_desempenio.get(oferta).get("col_name_m")))
+    desempenio_lengua = DechDesempenio.objects.using("detch").raw("select * from dech.desempenio('{}','{}','{}')".format(cueanexo,options_desempenio.get(oferta).get("table_name_l"),options_desempenio.get(oferta).get("col_name_l")))
     
 
-    for i,d in enumerate(desempenio):
+    for i,d in enumerate(desempenio_math):
         datasets.append(
             {
             "label": f"Nivel: {d.desempenio}",
@@ -59,8 +83,18 @@ def chart_data(request,oferta=None):
             },
         )
 
+    for i,d in enumerate(desempenio_lengua):
+        datasets.append(
+            {
+            "label": f"Nivel: {d.desempenio}",
+            "data": [d.promedio],
+            "backgroundColor": colors[i],
+            "stack":"Stack1"
+            },
+        )
+
     data = {
-        "labels": [f"Desempeño Matemática",],
+        "labels": ["Matemática","Lengua","Ciencias Naturales","Ciencias Sociales"],
         "datasets": datasets
     }
     print(data)
