5fddd1b39084596dbd2837f9105107d4e254c2dc
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-02-22 21:00:01 -0300

    agregado de custom admin para usuarios staff

--- estadisticas/users/models.py
+++ estadisticas/users/models.py
@@ -6,8 +6,16 @@ from django.contrib.auth.models import User
 
 
 class User(AbstractUser):
-    password_entered = models.BooleanField(default=False)
-    failed_login_attempts = models.IntegerField(default=0)
+    password_entered = models.BooleanField(
+        default=False,
+        verbose_name='Password cambiado',
+        help_text='Indica si la escuela eligió su password. El campo no debe estar marcado si desea permitirle que pueda elegir una nueva contraseña.'
+        )
+    failed_login_attempts = models.IntegerField(
+        default=0,
+        verbose_name='Cantidad de intentos fallidos',
+        help_text='Debe ser menor a 6 para que el usuario no sea bloqueado.'
+        )
 
     REQUIRED_FIELDS = ["password"]
 
